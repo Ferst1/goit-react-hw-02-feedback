e2dff232b86bdf32743c77c1719cf59408e0f9b7
Ferst1 <helenochka4@gmail.com>
2023-10-15 16:22:18 +0200

    vorbereitung

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// class Feedback extends React.Component {
+//   render() {
+//     return (
+//       <>
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//         bad: 0,
+//         </>
+//     );
+//   };
+// }
